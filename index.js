11e3059f93a1312b440c04fdb442bb271f996560
Tabsheera Shakeel <tabsheerasha123@gmail.com>
2024-04-04 10:12:37 +0500

    adding my 100 days challange(day2)to github

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 "use strict";
-//DAY ONE
-//Quesitin 1
-//installl node.js, Typescript and VS code on your computer/laptop.
+//DAY TWO
+//Quesition 1
+//Famous Quote:find a quote from a famous person you admire. print the quote and the name of its author.
+//your output should look like the following. including the quotation marks:
+//"Albert Einstein once said,"A person who never make a mistake never tried anything new.""
+console.log(`William Shakespeare once said, "uneasy lies the head that waers a crown"`);
 //Quesition 2
-//Personal Message: store a person's name in a varible and print a message to them, like "hello Asharib,
-//would you like to learn some Typescript today?"
-let Name = "tabsheera";
-console.log(`"hello ${Name}, would you like to learn some typescript today?"`);
-//Quesition 3
-//stripping name: store a person's name,and include some whitespace characters at the beginning and end 
-//of the name. Use"\t"(tab) and "\n"(new line) at least once.print the name once, so the whitespace around the
-//name is displayed. then print the name after srtippping the white spaces.
-let personName = "tabsheera";
-console.log(personName.toLowerCase());
-console.log(personName.toUpperCase());
-console.log(personName.charAt(0).toUpperCase() +
-    Name.slice(1).toLowerCase());
+//Famous Quote 2:Repeat excerise 1 but this time store a famous person's name in varible called Famous_person.
+//then compose your message and store it in a new varible called message.print your message.
+let Famous_person = "Alama Iqbal";
+let message = `${Famous_person} once said,"The ultimate aim of the ego is not to see something, but to be something."`;
+console.log(message);
+//QUESITION 3
+//Stripping Names:store a person's name, and include some whitespace character at the begining and end of the
+//name.Use "\t"(tab) and "\n"(new line) at least once.print the name once,so the whitespace around name
+//displayed.then print the name after stripping with white spaces.
+let Name = "\t\n Tabsheera \t\n";
+console.log(Name);
+console.log(Name.trim());
