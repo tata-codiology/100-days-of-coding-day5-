4d11504a3772fd24f6d96b2ffb607669d2a4c970
Tabsheera Shakeel <tabsheerasha123@gmail.com>
2024-04-15 14:05:45 +0500

    push 100 days of coding challange(day4) to github

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 "use strict";
-//DAY TWO
-//Quesition 1
-//Famous Quote:find a quote from a famous person you admire. print the quote and the name of its author.
-//your output should look like the following. including the quotation marks:
-//"Albert Einstein once said,"A person who never make a mistake never tried anything new.""
-console.log(`William Shakespeare once said, "uneasy lies the head that waers a crown"`);
-//Quesition 2
-//Famous Quote 2:Repeat excerise 1 but this time store a famous person's name in varible called Famous_person.
-//then compose your message and store it in a new varible called message.print your message.
-let Famous_person = "Alama Iqbal";
-let message = `${Famous_person} once said,"The ultimate aim of the ego is not to see something, but to be something."`;
-console.log(message);
-//QUESITION 3
-//Stripping Names:store a person's name, and include some whitespace character at the begining and end of the
-//name.Use "\t"(tab) and "\n"(new line) at least once.print the name once,so the whitespace around name
-//displayed.then print the name after stripping with white spaces.
-let Name = "\t\n Tabsheera \t\n";
-console.log(Name);
-console.log(Name.trim());
+//DAY FOUR !!!!
+//Quesition 10
+//adding comments:choose two of the program you've written,and add at least add one comment to each.if the 
+//program are straightforward at this point,just add your name and the current date at the top.write one sentence
+//discribing what each program does.
+//tabsheera 15/4/24
+//this code prints personal message
+let Name = "tabsheera";
+console.log(`hello, ${Name} would you like to learn some typescript today?`);
+//QUESITION 11
+//names:store the names of a few of your friends in an array called names. print each person's name by 
+//accessing each element in the list, one at a time
+let Names1 = ["faiza", "irma", "maha"];
+for (let i = 0; i < Names1.length; i++) {
+    console.log(Names1[i]);
+}
+;
+//QUESITION 12
+//greeting:use the array from ex:11. instead of just printing each person's name,print a message to them.
+//the message should be the same for eaxh person,nut personalized with their names
+let Names = ["faiza", "irma", "maha"];
+for (let name of Names) {
+    console.log(`hello ${Names},would you guys like to learn some typescript today?`);
+}
+;
